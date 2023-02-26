b457c340045c18b809a0b5a5d3b9c4fa6ae1bae6
Muhammad Mahmudun Nabi <mmnabi.cse@gmail.com>
2023-02-26 12:31:32 +0600

    added mudblazor, updated layout and nav

---
+++ StudentManagement.App/Shared/MainLayout.razor.cs
@@ -0,0 +1,16 @@
+﻿using MudBlazor;
+
+namespace StudentManagement.App.Shared
+{
+    public partial class MainLayout
+    {
+        private bool _drawerOpen = true;
+        private bool _isDarkMode;
+        private MudTheme _theme = new();
+
+        void DrawerToggle()
+        {
+            _drawerOpen = !_drawerOpen;
+        }
+    }
+}
