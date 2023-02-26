7efcec8a83c1e2e85526773627f60abd71abbb1a
Muhammad Mahmudun Nabi <mmnabi.cse@gmail.com>
2023-02-26 11:31:10 +0600

    blazor app added

--- StudentManagement.Api/Program.cs
+++ StudentManagement.Api/Program.cs
@@ -20,23 +20,29 @@ builder.Services.AddScoped<ICountryRepository, CountryRepository>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
 using (var scope = app.Services.CreateScope())
 {
     using var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
     context.Database.EnsureCreated();
 }
 
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseWebAssemblyDebugging();
+}
+
 app.UseHttpsRedirection();
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
 
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapFallbackToFile("index.html");
 
 app.Run();
